e85f9ae908fb51dc1bd72699a26c6589332cecb0
xffbigshark <45616865+xffbigshark@users.noreply.github.com>
2018-12-05 19:22:37 +0800

    Update TestGit.java

--- gitproject/src/main/java/com/atguigu/TestGit.java
+++ gitproject/src/main/java/com/atguigu/TestGit.java
@@ -6,7 +6,9 @@ package com.atguigu;
  */
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
+       
         System.out.println("给你吃颗糖");
+        System.out.println("再给你吃颗糖");
+        
     }
 }
