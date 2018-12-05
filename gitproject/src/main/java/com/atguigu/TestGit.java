2ddf13d01a44507a1723fdf2e6d5aa6cee6e24c5
sharkxff <735250895@qq.com>
2018-12-05 16:59:40 +0800

    push a class

---
+++ gitproject/src/main/java/com/atguigu/TestGit.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author zxfcode
+ * @create 2018-12-05 16:57
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAA");
+    }
+}
